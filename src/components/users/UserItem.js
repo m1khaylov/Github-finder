072c0ae14caff1b059eea63f8c2e55e2264b67dd
iMac <iMac@iMac-iMac.local>
2020-04-14 16:37:08 +0300

    refactoring App, Navbar, UserItem to functional components and install axios

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,29 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-    render() {
-        // const { login, avatar_url, html_url } = this.props.user;
-
-        return (
-            <div className='card text-center'>
-                <img
-                    src={this.props.user.avatar_url}
-                    alt='avatar'
-                    className='round-img'
-                    style={{ width: "60px" }}
-                />
-                <h3>{this.props.user.login}</h3>
-                <div>
-                    <a
-                        href={this.props.user.html_url}
-                        className='btn btn-dark btn-sm my-1'
-                    >
-                        More
-                    </a>
-                </div>
+const UserItem = (props) => {
+    return (
+        <div className='card text-center'>
+            <img
+                src={props.user.avatar_url}
+                alt='avatar'
+                className='round-img'
+                style={{ width: "60px" }}
+            />
+            <h3>{props.user.login}</h3>
+            <div>
+                <a
+                    href={props.user.html_url}
+                    className='btn btn-dark btn-sm my-1'
+                >
+                    More
+                </a>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
