55cba124b3ab62a1070c0659c58b0d59e38b1862
iMac <iMac@iMac-iMac.local>
2020-04-14 16:06:55 +0300

    Creating Users component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,23 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
-    state = {
-        id: "id",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-    };
-
     render() {
-        const { login, avatar_url, html_url } = this.state;
+        // const { login, avatar_url, html_url } = this.props.user;
 
         return (
             <div className='card text-center'>
                 <img
-                    src={avatar_url}
+                    src={this.props.user.avatar_url}
                     alt='avatar'
                     className='round-img'
                     style={{ width: "60px" }}
                 />
-                <h3>{login}</h3>
+                <h3>{this.props.user.login}</h3>
                 <div>
-                    <a href={html_url} className='btn btn-dark btn-sm my-1'>
+                    <a
+                        href={this.props.user.html_url}
+                        className='btn btn-dark btn-sm my-1'
+                    >
                         More
                     </a>
                 </div>
