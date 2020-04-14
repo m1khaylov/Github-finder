65a0f982b4ba6fb29d7a34fcf7e83c5c0415a123
iMac <iMac@iMac-iMac.local>
2020-04-14 18:49:49 +0300

    refactoring Users.js to functional component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,40 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
+import { Spinner } from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-    state = {
-        users: [
-            {
-                login: "mojombo",
-                id: 1,
-                avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-                html_url: "https://github.com/mojombo",
-            },
-            {
-                login: "defunkt",
-                id: 2,
-                avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-                html_url: "https://github.com/defunkt",
-            },
-            {
-                login: "pjhyett",
-                id: 3,
-                avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-                html_url: "https://github.com/pjhyett",
-            },
-        ],
-    };
+const Users = (props) => {
+    return props.loading ? (
+        <Spinner />
+    ) : (
+        <div style={userStyle}>
+            {props.users.map((user) => (
+                <UserItem key={user.id} user={user} />
+            ))}
+        </div>
+    );
+};
 
-    render() {
-        return (
-            <div style={userStyle}>
-                {this.state.users.map((user) => (
-                    <UserItem key={user.id} user={user} />
-                ))}
-            </div>
-        );
-    }
-}
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+};
 
 const userStyle = {
     display: "grid",
