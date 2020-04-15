e003584feaa923ea30b27da95a8ab2af1ada8e3d
iMac <iMac@iMac-iMac.local>
2020-04-15 15:03:58 +0300

    created component Alert.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,14 @@ import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import axios from "axios";
 import "./App.css";
+import { Alert } from "./components/layout/Alert";
 
 class App extends Component {
     state = {
         users: [],
         loading: false,
+        alert: null,
     };
-
-    // async componentDidMount() {
-    //     this.setState({ loading: true });
-
-    //     const res = await axios.get(
-    //         `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    //     );
-
-    //     this.setState({ users: res.data, loading: false });
-    // }
 
     // Search Github Users
     searchUsers = async (text) => {
@@ -35,17 +27,24 @@ class App extends Component {
     // Clear users from state
     clearUsers = () => this.setState({ users: [], loading: false });
 
-    showClear = () => {};
+    // Set Alert
+    setAlert = (msg, type) => {
+        this.setState({ alert: { msg: msg, type: type } });
+
+        setTimeout(() => this.setState({ alert: null }), 3000);
+    };
 
     render() {
         return (
             <div className='App'>
                 <Navbar />
                 <div className='container'>
+                    <Alert alert={this.state.alert} />
                     <Search
                         searchUsers={this.searchUsers}
                         clearUsers={this.clearUsers}
                         showClear={this.state.users.length > 0}
+                        setAlert={this.setAlert}
                     />
                     <Users
                         loading={this.state.loading}
