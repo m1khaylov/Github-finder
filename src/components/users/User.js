e7c0f6065fa750771ead62192674790a3d9991d1
iMac <iMac@iMac-iMac.local>
2020-04-16 17:22:20 +0300

    created components Repos and RepoItem

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,21 @@
 import React, { Fragment, Component } from "react";
 import { Spinner } from "../layout/Spinner";
+import { Repos } from "../repos/Repos";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 export class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
+        this.props.getUserRepos(this.props.match.params.login);
     }
 
     static propTypes = {
         loading: PropTypes.bool.isRequired,
         user: PropTypes.object.isRequired,
+        repos: PropTypes.array.isRequired,
         getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired,
     };
 
     render() {
@@ -113,6 +117,7 @@ export class User extends Component {
                         Public Gists: {public_gists}
                     </div>
                 </div>
+                <Repos repos={this.props.repos} />
             </Fragment>
         );
     }
